220ed765c79c9dc88f9d8bebc22cd5bc0ef09583
Darius <dandrada23@gmail.com>
2020-04-01 23:17:49 -0300

    commit de frecuencias.rb

--- frecuencias.rb
+++ frecuencias.rb
@@ -3,39 +3,25 @@ Escribe un método llamado frecuencia que reciba una cadena de texto
 y retorne un hash con el número de veces que aparece cada caracter 
 (exceptuando el espacio en blanco).
 =end
-def frecuencia(str)
-    result= {}
-    chars = str.chars
-    valor=0
-
+def frecuencia(str) #"hola mundo"
+    hash_result= {}
+    chars = str.chars #["h", "o", "l", "a", " ", "m", "u", "n", "d", "o"]
+    contador=1
     chars.each do |char|
-        if char
-
-        result[char] = valor
+        if hash_result.has_key?(char)                       
+            hash_result[char]= contador + 1
+            
+        else
+            hash_result[char]=contador
         end
-        result.select {|x,v| x =="a"}
-
-        
+                
     end
-    result
-    #result
-
-    #chars= str.chars
-
-
-    #chars.each do |char|
-    #char
-    #end
-    #str.(\w)
-
-    #end
-=begin cadena=""
-    cadena= str.scan(/(.)/) 
-=end
+    hash_result  
+    
 end
 
 puts frecuencia("hola mundo")
 # {"h"=>1, "o"=>2, "l"=>1, "a"=>1, "m"=>1, "u"=>1, "n"=>1, "d"=>1}
 
-#puts frecuencia("anita lava la tina")
+puts frecuencia("anita lava la tina")
 # {"a"=>6, "n"=>2, "i"=>2, "t"=>2, "l"=>2, "v"=>1}
